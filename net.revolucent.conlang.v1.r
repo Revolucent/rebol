ae24a5ba3672fceda8e8d2ad917494e0ea577242
gregory <gregory@6da31713-1fe4-40da-ab75-2dd36f75bfbe>
2010-05-25 19:24:23 +0000

    Finally got conlang working perfectly.
    git-svn-id: svn://svn.revolucent.net/Shared/REBOL/trunk@1090 6da31713-1fe4-40da-ab75-2dd36f75bfbe

--- net.revolucent.conlang.v1.r
+++ net.revolucent.conlang.v1.r
@@ -1,48 +1,140 @@
 REBOL [
 	type: module
 	name: net.revolucent.conlang
-	exports: [conlang]
+	exports: [parse-conlang]
 ]
 
-expression!: object [
-	parent: none
-	verb: 'join ; default verb for all expressions
-]
-
-conlang: object [
-		
-	parse: funct [
-		file [file!]
-	][			
-		
-		current: none
-		
-		push: func [
-			'spec [word! block!]
-		]
-				
-		rand-arg: [
-			string!
-		|	into [some [opt integer! expression]]
-		]		
-		
-		expression: [
-			'rand rand-arg
-		|	'rept integer! integer! expression
-		|	'join into [some expression]
-		|	word!
-		|	string!
-		]
-		
-		rules: [
-			some [
-				set-word!
-				expression
-			]
-		]
-		
-		system/contexts/system/parse load/unbound file rules
+conlang!: object [
+	
+	expression!: object [
+		parent: none
+		weight: 1
 	]
 	
-]
+	names: make map! []
 
+	make-rand: func [
+		/string
+			s [string!]
+		/local
+			spec
+	][
+		spec: copy []
+		either string [
+			append spec compose/deep [
+				eval: does [to string! random/only (s)]
+			]
+		][
+			append spec [
+				expressions: copy []
+				eval: has [e list] [
+					list: copy []
+					foreach e expressions [
+						append/dup list e e/weight
+					]
+					e: random/only list
+					e/eval
+				]
+			]
+		]
+		make expression! spec
+	]
+	
+	make-join: has [spec] [
+		make expression! [
+			expressions: copy []
+			eval: has [result] [
+				result: copy ""
+				foreach e expressions [
+					append result e/eval
+				]
+				result
+			]
+		]
+	]
+	
+	make-rept: func [
+		mn [integer!]
+		mx [integer!]
+	][
+		make expression! [
+			expressions: copy []
+			min-repeat: mn
+			max-repeat: mx
+			eval: has [e r result] [
+				result: copy ""
+				e: expressions/1
+				repeat n min-repeat [
+					append result e/eval
+				]
+				r: (random (max-repeat - min-repeat + 1)) - 1
+				repeat n r [
+					append result e/eval
+				]
+				result
+			]
+		]
+	]
+	
+	make-string: func [
+		value [string!]
+	][
+		make expression! [
+			eval: value
+		]
+	]
+	
+	make-ref: func [
+		value [word!]
+	][
+		make expression! [
+			ref: value
+			eval: does [
+				names/:ref/eval
+			]
+		]
+	]
+	
+	current: none
+	
+	push: func [
+		o [object!]
+	][
+		append current/expressions o
+		o/parent: current
+		current: o
+	]
+	
+	pop: does [
+		current: current/parent
+	]
+	
+	; Some temporary variables for parsing
+	e: n: mn: mx: name: none
+		
+	expression: [
+		'rand set e string! (push make-rand/string e pop)
+	|	'rand (push make-rand) into [some [(n: 1) opt [set n integer!] expression (set in last current/expressions 'weight n)]] (pop)
+	|	'rept set mn integer! set mx integer! (push make-rept mn mx) expression (pop)
+	|	'join (push make-join) into [some expression] (pop)
+	|	set e string! (push make-string e pop)
+	|	set e word! (push make-ref e pop)
+	]
+	
+	conlang: [
+		some [
+			set name set-word! (current: names/:name: make-join)
+			expression
+			(current: none)
+		]
+	]
+	
+]   
+
+parse-conlang: funct [
+	arg [file! string! block!]
+][
+	arg: either block? arg [arg] [load/unbound arg]
+	conlang: make conlang! none
+	either parse arg conlang/conlang [conlang/names/main] [none]
+]
